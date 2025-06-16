154d458ad6578449eedd49879893bd939dc0f00b
Tanuv Nair <tanuvnair@gmail.com>
2025-06-16 05:33:47 +0530

    Added a settings dialog comment

--- app/routes/dashboard/Dashboard.tsx
+++ app/routes/dashboard/Dashboard.tsx
@@ -643,6 +643,7 @@ export default function Dashboard() {
           </div>
         </div>
 
+        {/* Settings Dialog */}
         <Dialog
           open={isSettingsDialogOpen}
           onOpenChange={setIsSettingsDialogOpen}
